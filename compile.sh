72f105d1f9c801af607ca34ff82a54da57aa0ad7
David Mailhot <djmailhot@gmail.com>
2013-05-13 23:47:47 -0700

    compile.sh now puts .class files in correct place

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-javac -g -cp ./jars/plume.jar:./jars/java-json.jar lib/edu/washington/cs/cse490h/lib/*.java proj/*.java
+javac -g -cp ./jars/plume.jar:./jars/java-json.jar lib/edu/washington/cs/cse490h/lib/*.java proj/*.java -d bin
 cd lib
 jar cvf ../jars/lib.jar edu/washington/cs/cse490h/lib/*.class
 
